75e17589d476971d8c042847d71cfc008f9dfae4
Jhonny Quispe <jhonny.quispejl@gmail.com>
2021-05-20 21:23:03 -0500

    configuracion para la DB

--- src/index.js
+++ src/index.js
@@ -3,17 +3,25 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 
+const conectionDB = require('./db')
+
+// inicializamos la app
 const app = express()
+
+// llamamos a la conexion
+conectionDB()
 
 // middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
+// rutas
 app.get('/', (req, res) => {
 	res.status(200).send('Comenzamos api')
 })
 
+// lanzamos el servidor
 app.listen(process.env.PORT, () => {
 	console.log(`Server runing on http://localhost:${process.env.PORT}`)
 })
